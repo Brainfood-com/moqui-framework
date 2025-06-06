4f9620f3a5b029c463b94b5cecd9cf88315d8bad
acetousk <acetousk@users.noreply.github.com>
2025-06-06 12:00:30 -0600

    Add REST API artifact hit logging

--- framework/src/main/groovy/org/moqui/impl/service/RestApi.groovy
+++ framework/src/main/groovy/org/moqui/impl/service/RestApi.groovy
@@ -644,9 +644,9 @@ class RestApi {
 
             // push onto artifact stack, check authz
             String curPath = getFullPathName([])
-            ArtifactExecutionInfoImpl aei = new ArtifactExecutionInfoImpl(curPath, ArtifactExecutionInfo.AT_REST_PATH, getActionFromMethod(ec), null)
+            ArtifactExecutionInfoImpl aei = new ArtifactExecutionInfoImpl(curPath, ArtifactExecutionInfo.AT_REST_PATH, getActionFromMethod(ec), ec.web.getRequest().getMethod().toLowerCase())
             // for now don't track/count artifact hits for REST path
-            aei.setTrackArtifactHit(false)
+            aei.setTrackArtifactHit(true)
             // NOTE: consider setting parameters on aei, but don't like setting entire context, currently used for entity/service calls
             ec.artifactExecutionFacade.pushInternal(aei, !moreInPath ?
                     (requireAuthentication == null || requireAuthentication.length() == 0 || "true".equals(requireAuthentication)) : false, true)
